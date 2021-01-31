f98b4fb4364d429d4c791b170997436a4a3b695c
Efren Ulloa <eulloa345@gmail.com>
2021-01-30 20:56:45 -0600

    feat: blog summary

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import { getSortedPostsData } from '../lib/posts';
 type Post = {
   date: string;
   id: string;
+  summary: string;
   title: string;
 }
 
@@ -24,13 +25,14 @@ export default function Index({ posts }: IndexProps) {
       <About />
       {posts?.length > 0 &&
         <ol className="text-center space-y-6">
-          {posts.map(({ id, date, title }) => (
+          {posts.map(({ date, id, summary, title }) => (
             <Link href={`/posts/${id}`} key={`${date}-${title}`}>
               <a className="block">
                 <li className="p-2" key={`${date}-${title}`}>
-                  <div className="text-center">
-                    <h1 className="text-2xl font-bold">{title}</h1>
-                    <p className="text-sm font-light">{date}</p>
+                  <div>
+                    <h1 className="text-2xl font-bold text-center">{title}</h1>
+                    <p className="text-sm font-light pb-5 text-center">{date}</p>
+                    <p className="font-light">{summary}</p>
                   </div>
                 </li>
               </a>
--- posts/first.md
+++ posts/first.md
@@ -1,5 +1,6 @@
 ---
 title: 'My first blog post'
 date: '2020-25-12'
+summary: 'First blog post!'
 ---
 This is my first blog post!
