bc9564f9db6acf2566e336aac6d75764b635e055
Efren Ulloa <eulloa345@gmail.com>
2021-01-31 22:08:29 -0600

    feat: search functionality

--- components/header.tsx
+++ components/header.tsx
@@ -1,24 +1,41 @@
+import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Header() {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const handleChange = (e: any) => setSearchValue(e.target.value);
+
   return (
-    <header className="flex">
+    <header className="flex pb-6 md:pb-0">
       <Link href="/">
         <a href="#">
           <img src="/images/kev.jpg" alt="Kev" />
         </a>
       </Link>
       <div className="flex justify-end w-full">
-        <ul className="flex space-x-5">
-          <li>
+        <ul className="block md:flex space-x-5">
+          <li className="text-right pb-2 md:pb-0">
             <Link href="/fitness">
               <a href="#">Fitness</a>
             </Link>
           </li>
-          <li>
+          <li className="text-right pb-2 md:pb-0">
             <Link href="/investing">
               <a href="#">Investing</a>
             </Link>
+          </li>
+          <li>
+            <form className="border search flex" method="GET" action="/search">
+              <input
+                className="p-2"
+                name="q"
+                placeholder="Search"
+                type="text"
+                value={searchValue}
+                onChange={handleChange}
+              />
+              <input type="submit" value="🔍" disabled={!searchValue} />
+            </form>
           </li>
         </ul>
       </div>
---
+++ pages/search.tsx
@@ -0,0 +1,52 @@
+import Layout from '../components/layout';
+import Link from 'next/link';
+import { getSearchedPosts } from '../lib/posts';
+import { Post } from '../types';
+import { useRouter } from 'next/router';
+
+export default function Search({ searchedPosts }: any) {
+  const router = useRouter();
+  const searchedTerm = router?.query?.q;
+
+  if (!searchedPosts || !searchedPosts.length) {
+    return (
+      <Layout>
+        <div className="text-center">
+          <h1 className="text-2xl font-bold">Oops!</h1>
+          <p>
+            {' '}
+            Your search term <em>{`${searchedTerm}`}</em> did not return any
+            results! Please try again.
+          </p>
+        </div>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <ol className="text-center space-y-6">
+        {searchedPosts.map(({ date, id, title }: Post) => (
+          <Link href={`/posts/${id}`} key={`${date}-${title}`}>
+            <a className="block">
+              <li>
+                <div>
+                  <h1 className="text-2xl font-bold text-center">{title}</h1>
+                  <p className="text-sm font-light pb-5 text-center">{date}</p>
+                </div>
+              </li>
+            </a>
+          </Link>
+        ))}
+      </ol>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query }: any) {
+  return {
+    props: {
+      searchedPosts: await getSearchedPosts(query.q),
+    },
+  };
+}
