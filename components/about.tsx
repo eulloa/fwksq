a5605b7be0dda8a8b9c200ab951a3b5dde6de76e
Efren Ulloa <eulloa345@gmail.com>
2021-05-23 15:06:01 -0500

    feat: about me and first post

--- components/about.tsx
+++ components/about.tsx
@@ -3,9 +3,22 @@ export default function About() {
     <div className="dark:text-white">
       <h1 className="text-5xl font-bold pb-4 text-center">K</h1>
       <div className="flex justify-center pb-5">
-        <p className="font-light w-60">
-          An informative and educational resource focused on fitness, motivation
-          and financial success.
+        <p className="font-light w-90">
+          Hello fellow friends, family and investors. My name is Kevin Kim and I
+          want to say thank you for checking out my site! I am a Korean American
+          who came to America with my family when I was 7 years old. From as far
+          back as I can remember, my parents always told me life was better in
+          America. A common refrain, no doubt heard by millions of other kids
+          since. Eventually, I found out that all the stuff we learn are tools
+          that when learned correctly, have the capacity to greatly improve our
+          lives. As I learned more and became more adept at using some of these
+          tools, I decided to write this blog to document my experiences and
+          perspective on topics like stocks, real estate, life insurance, health
+          savings and flex spending accounts and other investment vehicles.
+          You’ll quickly come to find that not all of these tools are
+          appropriate for everyone. Whatever tool or tools are right for you, I
+          want to help teach you how to use them effectively so you can further
+          enrich your life and start making your money work for you.
         </p>
       </div>
     </div>
